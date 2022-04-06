c945812bb0992977f4e09e5cab92e3779fb63a43
Augusto Braga <bragaaugusto361@gmail.com>
2022-04-05 21:23:06 -0300

    inicio CRUD banco de dados

---
+++ aula-031/routes.js
@@ -0,0 +1,29 @@
+const pool = require("./configDb");
+
+class Routes {
+  async registerRoute(req, res) {
+    const newUser = req.body;
+
+    try {
+      await pool.query(
+        `INSERT INTO users(username, email, password) VALUES('${newUser.username}', '${newUser.email}', '${newUser.password}')`
+      );
+
+      console.log(newUser);
+
+      res.json("OK");
+    } catch (e) {
+      if (e.code == "23505") return res.status(400).json("Email já existente");
+    }
+  }
+
+  async showUsers(req, res) {
+    const allUsers = await pool.query("SELECT * FROM users");
+
+    res.json(allUsers.rows);
+  }
+}
+
+const routes = new Routes();
+
+module.exports = routes;
---
+++ aula-031/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", routes.registerRoute);
+app.get("/show", routes.showUsers);
+
+app.listen(3333, () => {
+  console.log("Server is running!");
+});
